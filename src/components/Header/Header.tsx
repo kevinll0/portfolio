804d2ddbf1f57c6eb618145827214e4ceaeef4df
kevinllo <kevingrande2002@gmail.com>
2023-04-14 18:53:35 -0600

    ADD: Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Toggle } from '../ToggleDark/Toggle';
+import { AiOutlineAlignRight, AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'gatsby';
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <header>
-            <div className="header-logo">
-                <h1>@kevinllo</h1>
+        <header className="flex justify-between items-center relative container mx-auto py-3">
+            <div className="flex items-center">
+                <div className="header-logo">
+                    <h1 className="text-2xl font-bold">@kevinllo</h1>
+                </div>
             </div>
-            <div>
-                <Toggle />
+
+            <nav className="hidden sm:block">
+                <ul className="flex space-x-4 items-center">
+                    <li className='font-semibold'> <Link to="/about">About</Link></li>
+                    <li className='font-semibold'>  <Link to="/projects">Projects</Link></li>
+                    <li className='font-semibold'><Link to="/contact">Contact</Link></li>
+                    <li className='font-semibold'> <Toggle /></li>
+                </ul>
+            </nav>
+
+            <div className="sm:hidden z-20" onClick={() => setIsMenuOpen(!isMenuOpen)} >
+                {!isMenuOpen
+                    ? <AiOutlineAlignRight fontSize='30px'
+                        className={`cursor-pointer ${isMenuOpen ? 'text-white' : 'light:text-black'}`} />
+                    : <AiOutlineClose fontSize='30px'
+                        className={`cursor-pointer ${isMenuOpen && 'text-white'}`} />}
+            </div>
+
+            <div className={isMenuOpen
+                ? 'mobile-menu sm:hidden absolute inset-0 text-slate-100 dark:text-accent bg-gray-900 opacity-90 z-10 min-h-screen flex justify-center items-center'
+                : 'mobile-menu hidden sm:hidden'}>
+                <nav className="mt-4">
+                    <ul className="grid gap-4">
+                        <li className='text-2xl'> <Link to="/about">About</Link></li>
+                        <li className='text-2xl'>  <Link to="/projects">Projects</Link></li>
+                        <li className='text-2xl'><Link to="/contact">Contact</Link></li>
+                        <li className='text-2xl'> <Toggle /></li>
+                    </ul>
+                </nav>
             </div>
         </header>
+
     );
 }
 
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 }
 function Layout({ children }: LayoutProps) {
     return (
-        <div className='bg-slate-100 dark:bg-primary dark:text-primary h-screen flex flex-col'>
+        <div className='bg-slate-100 dark:bg-primary dark:text-secondary h-screen flex flex-col -z-0 relative'>
             <Header />
             <main className='flex-1'>
                 {children}
