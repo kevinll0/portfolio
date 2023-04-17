4fc105a7f142ff17e6f87e9b4579966b015f3cca
kevinllo <kevingrande2002@gmail.com>
2023-04-17 08:51:28 -0600

    ADD: gastby trasnform json plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,11 +14,18 @@ const config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
       },
     },
   ]
