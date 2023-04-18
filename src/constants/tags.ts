9ee8c4d2ce0abed666e3b9a3d323a2380748da68
kevinllo <kevingrande2002@gmail.com>
2023-04-17 21:34:38 -0600

    ADD: filter projects by tags

---
+++ src/constants/tags.ts
@@ -0,0 +1,13 @@
+const TAGS = [
+    'HTML',
+    'CSS',
+    'SASS',
+    'TailwindCSS',
+    'Javascript',
+    'Typescript',
+    'ReactJs',
+    'Redux',
+    'GatsbyJs'
+]
+
+export default TAGS;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layouts';
 import { HeadFC, graphql, navigate, useStaticQuery } from 'gatsby';
 import { GatsbyImage, ImageDataLike, getImage } from 'gatsby-plugin-image';
 import IProjects from '../interfaces/IProjects';
+import TAGS from '../constants/tags';
 
 function Projects() {
     const data: IProjects = useStaticQuery(graphql`
@@ -25,13 +26,41 @@ function Projects() {
         }
     }
     `);
+    const [tagsSelected, setTagsSelected] = useState<string[]>([]);
+    const handleTagsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (tagsSelected.includes(e.currentTarget.value)) {
+            const newTag = tagsSelected.filter(tag => tag !== e.currentTarget.value);
+            setTagsSelected([...newTag]);
+            return;
+        }
+        setTagsSelected([...tagsSelected, e.currentTarget.value]);
+    }
+    const getProjects = () => {
+        let projects = data.allProjectsJson.edges;
+        if (tagsSelected.length > 0) {
+            projects = projects.filter(({ node }) => {
+                return tagsSelected.every(tag => node.tags.includes(tag));
+            });
+        }
+        return projects;
+    }
+
     return (
         <Layout>
             <section className="py-12">
                 <div className="container mx-auto px-3">
                     <h2 className="text-3xl font-bold mb-8">Projects</h2>
+                    <div className="flex flex-wrap mb-4">
+                        {TAGS.map(tag => (
+                            <label className="inline-flex items-center mr-4 mb-2" key={tag}>
+                                <input type="checkbox" className="form-checkbox h-5 w-5 text-indigo-600" value={tag} onChange={handleTagsChange} />
+                                <span className="ml-2 font-semibold">{tag}</span>
+                            </label>
+                        ))}
+                    </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                        {data.allProjectsJson.edges.map(({ node }) => (
+
+                        {getProjects().map(({ node }) => (
                             <div className="bg-slate-200 dark:bg-slate-800 rounded cursor-pointer" key={node.id} onClick={() => navigate('/projects/' + node.slug)}>
                                 <GatsbyImage image={getImage(node.image as ImageDataLike)!} alt={node.title} loading='lazy' />
                                 <h3 className=" px-4 text-lg font-medium mb-2">{node.title}</h3>
